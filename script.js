5b3f461d712cdfefaaf0d57211621b9563d28510
Elvis Chegegit config --global user.name Elvis <elvischege98@gmail.com>
2026-01-31 23:49:17 +0300

    Addition of year form and familiarisation of content to javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+function generateAkanName(){
+    const day = Number(document.getElementById(Day of Birth).value);
+    const month = String(document.getElementById(Month born).value);
+    const year = Number(document.getElementById(Year).value);
+    const gender = String(document.getElementById(Gender.value).value);
+}
+
