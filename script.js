19f7b1cba066371dbc333452150c7d6d911ebc44
Elvis Chegegit config --global user.name Elvis <elvischege98@gmail.com>
2026-02-01 23:57:26 +0300

    Javascript addition of onclick

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function generateAkanName(){
     const day = Number(document.getElementById(Day of Birth).value);
     const month = String(document.getElementById(Month born).value);
     const year = Number(document.getElementById(Year).value);
-    const gender = String(document.getElementById(Gender.value).value);
+    const gender = String(document.getElementById(Gender).value);
 }
 
+Object
