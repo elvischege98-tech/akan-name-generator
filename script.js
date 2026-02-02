41049049ed7906dbf4982f29f2d1956f8275a4a8
Elvis Chegegit config --global user.name Elvis <elvischege98@gmail.com>
2026-02-02 11:45:18 +0300

    Added Akan names

--- script.js
+++ script.js
@@ -4,4 +4,21 @@ function generateAkanName() {
     const year= Number(document.getElementById("Year").value)
     const gender= document.getElementById("Gender").value;
     const result= document.getElementById("Result")
-}
+};
+
+//Akan names
+const akanNames ={
+    Sunday: { male: "Kwasi", female: "Akosua"},
+    Monday: { male: "Kwadwo", female: "Adwao"},
+    Tuesday: { male: "Kwabena", female: "Abenaa"},
+    Wednesday: { male: "Kwaku", female: "Akua"},
+    Thursday: { male: "Yaw", female: "Yaa"},
+    Friday: { male: "Kofi", female: "Afua"},
+    Saturday: { male: "Kwame", female: "Ama"},
+};
+
+
+
+
+
+
