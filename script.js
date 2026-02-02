e5d0c72401b04f71cae73037f09909e78155c38d
Elvis Chegegit config --global user.name Elvis <elvischege98@gmail.com>
2026-02-02 13:34:40 +0300

    Gave instructions to months days and years

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-function generateAkanName() {
-    const day= Number(document.getElementById("Day").value);
-    const month= document.getElementById("Month").value;
-    const year= Number(document.getElementById("Year").value)
-    const gender= document.getElementById("Gender").value;
-    const result= document.getElementById("Result")
-};
+document.getElementById("akanForm").addEventListener("submit", function (e) {
+    e.preventDefault();
 
-//Akan names
-const akanNames ={
-    Sunday: { male: "Kwasi", female: "Akosua"},
-    Monday: { male: "Kwadwo", female: "Adwao"},
-    Tuesday: { male: "Kwabena", female: "Abenaa"},
-    Wednesday: { male: "Kwaku", female: "Akua"},
-    Thursday: { male: "Yaw", female: "Yaa"},
-    Friday: { male: "Kofi", female: "Afua"},
-    Saturday: { male: "Kwame", female: "Ama"},
-};
+    const day = parseInt (document.getElementById("day").value);
+    const month = parseInt (document.getElementById("month").value);
+    const year = parseInt (Document.getElementById("year").value);
+    const gender = document.querySelector('input[name="gender"]:checked')?.value;
 
+    //Validation
+    if (!day || !month || !year || !gender) {
+        alert("Fill in all fields and select a gender");
+        return;
+    }
 
+    if(day < 1 || day > 31) {
+alert("Day must be between 1 and 31");
+return;
+    }
 
+    if("month < 1 || month > 12") {
+        alert("Month must be between 1 and 12");
+        return;
+    }
 
+    const february = month(2) {
+        if(february < 1 || february > 29){
+            alert(Must be between 1 and 29)
+            return;
+        }
+    }
 
+    //Akan names
+    const maleNames = ["Kwame","Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi"];
+    const femaleNames = ["Ama","Akosua","Adwoa","Abenna","Akua","Yaa","Afua"];
+    const days = ["Saturday","Sunday","Monday","Tuesday","Wednesday","Thusrday","Friday"];
 
+    //math
+    
